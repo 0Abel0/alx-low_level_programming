28bd60141908f7e93585ab7999f52d14c30b160e
Abel Gideon <abelgideon16@gmail.com>
2022-03-11 15:54:33 +0300

    Add last digit file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ printf("Last digit of %d and is less than 6 and not 0\n",n);
 }
 else if (n == 0)
 {
-printf("Last digit of %d and is 0\n");
+printf("Last digit of %d and is 0\n",n);
 }	  
 return (0);
 }
