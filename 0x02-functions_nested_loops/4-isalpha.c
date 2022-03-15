f3c002332b4659aff880faef3e4ad8b3a4c8f002
Abel Gideon <abelgideon16@gmail.com>
2022-03-15 21:48:56 +0300

    Add is alpha file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - checks if letter
+ * Return: 1 if letter 0 if not.
+ * @c: parameter
+ */
+int _isalpha(int c)
+{
+if (97 >= c <= 122 || 65 >= c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
