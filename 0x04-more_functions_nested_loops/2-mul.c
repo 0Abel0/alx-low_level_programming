4b6081185ae02cfa8dfdc9a745bccabed19ae795
Abel Gideon <abelgideon16@gmail.com>
2022-03-17 11:25:00 +0300

    Add mul file and update header file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplys two integers.
+ * @a: parameter.
+ * @b: parameter
+ * Return: multiplication of a and b.
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
