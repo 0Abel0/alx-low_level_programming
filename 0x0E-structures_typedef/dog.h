3e5575a79001ce99aab4a22e488321fa6993c488
Abel Gideon <abelgideon16@gmail.com>
2022-04-11 18:56:54 +0300

    Edit dog file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
+
 struct dog {
-char *name;
-char *owner;
-float age;
+    char *name;
+    char *owner;
+    float age;
 };
